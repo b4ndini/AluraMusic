9f44ada9134c6408037562acfbbd1ee66232326e
Luis Felipe Pires Monte Maior <luis.maior@procempa.com.br>
2025-07-04 01:33:20 -0300

    Estrutura inicial

---
+++ src/main/java/com/lfelipe/alura/music/AluraMusicApplication.java
@@ -0,0 +1,18 @@
+package com.lfelipe.alura.music;
+
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AluraMusicApplication implements CommandLineRunner {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AluraMusicApplication.class, args);
+	}
+
+	@Override
+	public void run(String... args) throws Exception {
+		System.out.println("Alura Musicccccccccccccccccccccccccccccc");
+	}
+}
---
+++ src/test/java/com/lfelipe/alura/music/AluraMusicApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lfelipe.alura.music;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AluraMusicApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
